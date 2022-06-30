53fad9e8e606e0055d35ae62c9f1a8560528ea24
rafaelrc7 <contact@rafaelrc.com>
2022-06-30 01:35:51 -0300

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,37 @@
+{
+  description = "Bot AI for the 22.1 INF1771 AI competition";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils = {
+      url = "github:numtide/flake-utils";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    let lastModifiedDate = self.lastModifiedDate or self.lastModified or "19700101";
+        version = builtins.substring 0 8 self.lastModifiedDate;
+    in flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = import nixpkgs { inherit system; };
+          pkg = pkgs.buildGoModule {
+            pname = "inf1771-battlebot";
+            inherit version;
+            src = ./src;
+            vendorSha256 = "sha256-pQpattmS9VmO3ZIQUFn66az8GSmB4IvYhTTCFn6SUmo=";
+          };
+      in {
+        defaultPackage = pkg;
+        devShell = pkgs.mkShell {
+          buildInputs = [
+            pkgs.go
+            pkgs.gotools
+            pkgs.gopls
+            pkgs.gopkgs
+          ];
+        };
+      }
+    );
+
+}
+
