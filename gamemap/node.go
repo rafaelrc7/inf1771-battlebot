aeb2443c70c8c4659b746ee14d8a8a3743ed16bc
rafaelrc7 <contact@rafaelrc.com>
2022-07-08 01:40:08 -0300

    Change adjacent position priority

--- gamemap/node.go
+++ gamemap/node.go
@@ -5,41 +5,41 @@ func (m *Map) GetAdjacentPositions(pos Coord) (adjs []Coord) {
 
 	switch pos.D {
 	case NORTH:
-		adjs = append(adjs, Coord{pos.X, pos.Y, WEST})
-		adjs = append(adjs, Coord{pos.X, pos.Y, EAST})
-		if pos.Y < m.Height-1 {
-			adjs = append(adjs, Coord{pos.X, pos.Y + 1, pos.D})
-		}
 		if pos.Y > 0 {
 			adjs = append(adjs, Coord{pos.X, pos.Y - 1, pos.D})
 		}
+		if pos.Y < m.Height-1 {
+			adjs = append(adjs, Coord{pos.X, pos.Y + 1, pos.D})
+		}
+		adjs = append(adjs, Coord{pos.X, pos.Y, WEST})
+		adjs = append(adjs, Coord{pos.X, pos.Y, EAST})
 	case EAST:
-		adjs = append(adjs, Coord{pos.X, pos.Y, NORTH})
-		adjs = append(adjs, Coord{pos.X, pos.Y, SOUTH})
 		if pos.X < m.Width-1 {
 			adjs = append(adjs, Coord{pos.X + 1, pos.Y, pos.D})
 		}
 		if pos.X > 0 {
 			adjs = append(adjs, Coord{pos.X - 1, pos.Y, pos.D})
 		}
+		adjs = append(adjs, Coord{pos.X, pos.Y, NORTH})
+		adjs = append(adjs, Coord{pos.X, pos.Y, SOUTH})
 	case SOUTH:
-		adjs = append(adjs, Coord{pos.X, pos.Y, EAST})
-		adjs = append(adjs, Coord{pos.X, pos.Y, WEST})
-		if pos.Y > 0 {
-			adjs = append(adjs, Coord{pos.X, pos.Y - 1, pos.D})
-		}
 		if pos.Y < m.Height-1 {
 			adjs = append(adjs, Coord{pos.X, pos.Y + 1, pos.D})
 		}
+		if pos.Y > 0 {
+			adjs = append(adjs, Coord{pos.X, pos.Y - 1, pos.D})
+		}
+		adjs = append(adjs, Coord{pos.X, pos.Y, EAST})
+		adjs = append(adjs, Coord{pos.X, pos.Y, WEST})
 	case WEST:
-		adjs = append(adjs, Coord{pos.X, pos.Y, SOUTH})
-		adjs = append(adjs, Coord{pos.X, pos.Y, NORTH})
 		if pos.X > 0 {
 			adjs = append(adjs, Coord{pos.X - 1, pos.Y, pos.D})
 		}
 		if pos.X < m.Width-1 {
 			adjs = append(adjs, Coord{pos.X + 1, pos.Y, pos.D})
 		}
+		adjs = append(adjs, Coord{pos.X, pos.Y, SOUTH})
+		adjs = append(adjs, Coord{pos.X, pos.Y, NORTH})
 	}
 
 	return adjs
